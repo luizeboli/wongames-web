9fad9e42fee6cd83bc97024343d9c2cf426f5f1c
luizeboli <unifelicio@gmail.com>
2020-08-31 14:37:29 -0300

    feat: add heading component

---
+++ src/components/Heading/styles.ts
@@ -0,0 +1,40 @@
+import styled, { css, DefaultTheme } from 'styled-components';
+import media from 'styled-media-query';
+
+import { HeadingProps } from '.';
+
+const containerModifiers = {
+  lineLeft: (theme: DefaultTheme) => css`
+    padding-left: ${theme.spacings.xxsmall};
+    border-left: 0.7rem solid ${theme.colors.secondary};
+  `,
+
+  lineBottom: (theme: DefaultTheme) => css`
+    position: relative;
+    margin-bottom: ${theme.spacings.medium};
+
+    &::after {
+      content: '';
+      position: absolute;
+      left: 0;
+      bottom: -0.5rem;
+      width: 5rem;
+      border-bottom: 0.5rem solid ${theme.colors.primary};
+    }
+  `,
+};
+
+export const Container = styled.h2<HeadingProps>`
+  ${({ theme, color, lineLeft, lineBottom }) => css`
+    color: ${theme.colors[color!]};
+    font-size: ${theme.font.sizes.xlarge};
+
+    ${lineLeft && containerModifiers.lineLeft(theme)}
+    ${lineBottom &&
+    containerModifiers.lineBottom(theme)}
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge};
+    `}
+  `}
+`;
---
+++ src/components/Heading/test.tsx
@@ -0,0 +1,51 @@
+import { renderWithTheme } from 'utils/tests/helpers';
+import { screen } from '@testing-library/react';
+
+import 'jest-styled-components';
+import Heading from '.';
+
+describe('<Heading />', () => {
+  it('should render a white heading by default', () => {
+    // Render the component
+    renderWithTheme(<Heading>Won Games Title</Heading>);
+
+    // Select element to be tested
+    // Assert
+    expect(
+      screen.getByRole('heading', { name: /Won Games Title/i }),
+    ).toHaveStyle({
+      color: '#FAFAFA',
+    });
+  });
+
+  it('should render a black heading when color is passed', () => {
+    renderWithTheme(<Heading color="black">Won Games Title</Heading>);
+
+    expect(
+      screen.getByRole('heading', { name: /Won Games Title/i }),
+    ).toHaveStyle({
+      color: '#030517',
+    });
+  });
+
+  it('should render a heading with a line to the left side', () => {
+    renderWithTheme(<Heading lineLeft>Won Games Title</Heading>);
+
+    expect(
+      screen.getByRole('heading', { name: /Won Games Title/i }),
+    ).toHaveStyle({
+      'padding-left': '0.8rem',
+      'border-left': '0.7rem solid #3CD3C1',
+    });
+  });
+
+  it('should render a heading with a bottom line', () => {
+    renderWithTheme(<Heading lineBottom>Won Games Title</Heading>);
+
+    expect(
+      screen.getByRole('heading', { name: /Won Games Title/i }),
+    ).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
+      modifier: '::after',
+    });
+  });
+});
