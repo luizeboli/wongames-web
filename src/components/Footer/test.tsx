35e021427954e7f5adc90370a83f1e5cf406e86e
luizeboli <unifelicio@gmail.com>
2020-09-10 15:33:05 -0300

    feat: add footer component

---
+++ src/components/Footer/test.tsx
@@ -0,0 +1,23 @@
+import { renderWithTheme } from 'utils/tests/helpers';
+import { screen } from '@testing-library/react';
+
+import Footer from '.';
+
+describe('<Footer />', () => {
+  it('should render 4 columns', () => {
+    const { container } = renderWithTheme(<Footer />);
+
+    expect(
+      screen.getByRole('heading', { name: /Contact Us/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /Follow Us/i }),
+    ).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /Links/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /Location/i }),
+    ).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
