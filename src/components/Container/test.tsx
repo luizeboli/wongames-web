cb3b7b0c293ab66db84d76cab4a5b8f4ef9870d2
luizeboli <unifelicio@gmail.com>
2020-10-05 16:02:32 -0300

    feat: add container component

---
+++ src/components/Container/test.tsx
@@ -0,0 +1,39 @@
+import { screen } from '@testing-library/react';
+
+import theme from 'styles/theme';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import { Container } from '.';
+
+describe('<Container />', () => {
+  it('should ', () => {
+    const { container } = renderWithTheme(
+      <Container>
+        <span>Won Games</span>
+      </Container>,
+    );
+
+    expect(container.firstChild).toHaveStyleRule(
+      'max-width',
+      theme.grid.container,
+    );
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        max-width: 130rem;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: calc(NaN);
+        padding-right: calc(NaN);
+      }
+
+      <div
+        class="c0"
+      >
+        <span>
+          Won Games
+        </span>
+      </div>
+    `);
+  });
+});
