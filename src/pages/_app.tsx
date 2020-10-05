a76a20a5d26bc7c080463a84582c0d2433445b6c
luizeboli <unifelicio@gmail.com>
2020-10-05 16:29:04 -0300

    refactor: change html title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,14 +9,11 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>Hypa - NextJS Boilerplate</title>
+        <title>Won Games</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and styled-components"
-        />
+        <meta name="description" content="The best game store in the world " />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
