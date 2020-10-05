6040c78a8cf7d024bc608e5a9ecb2f9d1b5d5eae
luizeboli <unifelicio@gmail.com>
2020-10-05 15:02:28 -0300

    test: add slider tests

--- src/components/Slider/test.tsx
+++ src/components/Slider/test.tsx
@@ -1,5 +1,23 @@
 import { render, screen } from '@testing-library/react';
 
+import 'matchMediaMock';
 import Slider from '.';
 
-describe('<Slider />', () => {});
+describe('<Slider />', () => {
+  it('should render children as slider item', () => {
+    const { container } = render(
+      <Slider settings={{ slidesToShow: 2, infinite: false }}>
+        <p>Item 1</p>
+        <p>Item 2</p>
+      </Slider>,
+    );
+
+    expect(
+      screen.getByText(/item 1/i).parentElement?.parentElement,
+    ).toHaveClass('slick-slide');
+    expect(
+      screen.getByText(/item 2/i).parentElement?.parentElement,
+    ).toHaveClass('slick-slide');
+    expect(container).toMatchSnapshot();
+  });
+});
