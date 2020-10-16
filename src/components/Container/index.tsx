dbdeb3b050dcaea2011d3730bf465e01a8d30ff4
luizeboli <unifelicio@gmail.com>
2020-10-16 12:15:24 -0300

    build: fix type errors

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -5,7 +5,7 @@ export const Container = styled.div`
     max-width: ${theme.grid.container};
     margin-left: auto;
     margin-right: auto;
-    padding-left: calc(${theme.grid.gutter / 2});
-    padding-right: calc(${theme.grid.gutter / 2});
+    padding-left: calc(${theme.grid.gutter} / 2);
+    padding-right: calc(${theme.grid.gutter} / 2);
   `}
 `;
--- src/components/GameCardSlider/index.tsx
+++ src/components/GameCardSlider/index.tsx
@@ -6,7 +6,7 @@ import Slider, { SliderSettings } from 'components/Slider';
 
 import * as S from './styles';
 
-type GameCardSliderProps = {
+export type GameCardSliderProps = {
   items: GameCardProps[];
   color?: 'white' | 'black';
 };
