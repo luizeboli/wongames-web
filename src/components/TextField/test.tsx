b4e9a5febf9588cbca871829b843edb3afba991d
luizeboli <unifelicio@gmail.com>
2020-10-19 14:39:53 -0300

    feat: add textfield component

---
+++ src/components/TextField/test.tsx
@@ -0,0 +1,60 @@
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import TextField from '.';
+
+describe('<TextField />', () => {
+  it('should render with Label', () => {
+    renderWithTheme(<TextField label="Label" labelFor="Field" id="Field" />);
+
+    expect(screen.getByLabelText('Label')).toBeInTheDocument();
+  });
+
+  it('should render without Label', () => {
+    renderWithTheme(<TextField />);
+
+    expect(screen.queryByLabelText('Label')).not.toBeInTheDocument();
+  });
+
+  it('should render with placeholder', () => {
+    renderWithTheme(<TextField placeholder="hey you" />);
+
+    expect(screen.getByPlaceholderText('hey you')).toBeInTheDocument();
+  });
+
+  it('should change its value when typing', async () => {
+    const onInput = jest.fn();
+    renderWithTheme(
+      <TextField
+        onInput={onInput}
+        label="TextField"
+        labelFor="TextField"
+        id="TextField"
+      />,
+    );
+
+    const input = screen.getByRole('textbox');
+    const text = 'This is my new text';
+    userEvent.type(input, text);
+
+    await waitFor(() => {
+      expect(input).toHaveValue(text);
+      expect(onInput).toHaveBeenCalledTimes(text.length);
+    });
+    expect(onInput).toHaveBeenCalledWith(text);
+  });
+
+  it('should be accessible by tab', () => {
+    renderWithTheme(
+      <TextField label="TextField" labelFor="TextField" id="TextField" />,
+    );
+
+    const input = screen.getByLabelText('TextField');
+    expect(document.body).toHaveFocus();
+
+    userEvent.tab();
+    expect(input).toHaveFocus();
+  });
+});
