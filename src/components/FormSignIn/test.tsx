ed9d8f51fb44029ac24fcaf59f45d94d0f913360
luizeboli <unifelicio@gmail.com>
2020-11-05 12:27:08 -0300

    test: improve sign in tests

--- src/components/FormSignIn/test.tsx
+++ src/components/FormSignIn/test.tsx
@@ -6,13 +6,15 @@ import FormSignIn from '.';
 
 describe('<FormSignIn />', () => {
   it('should render the form', () => {
-    renderWithTheme(<FormSignIn />);
+    const { container } = renderWithTheme(<FormSignIn />);
 
     expect(screen.getByPlaceholderText(/e-mail/i)).toBeInTheDocument();
     expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument();
     expect(
       screen.getByRole('button', { name: /sign in/i }),
     ).toBeInTheDocument();
+
+    expect(container.parentElement).toMatchSnapshot();
   });
 
   it('should render the forgot password link', () => {
