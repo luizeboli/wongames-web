0eaa93210292cc85d4259f00cac06d026e16a612
luizeboli <unifelicio@gmail.com>
2020-11-05 11:40:30 -0300

    test: update logo tet

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -17,9 +17,9 @@ describe('<Logo />', () => {
   });
 
   it('should render the logo with id passed', () => {
-    const { container } = renderWithTheme(<Logo id="id" />);
+    const { container } = renderWithTheme(<Logo id="myId" />);
 
-    expect(container.querySelector('#paint_linear_id')).toBeInTheDocument();
+    expect(container.querySelector('#paint0_linear_myId')).toBeInTheDocument();
   });
 
   it('should render a black label when color is passed', () => {
