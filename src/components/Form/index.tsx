7600b21e640cb9efad41e0de760a395d47272662
luizeboli <unifelicio@gmail.com>
2020-11-05 13:13:02 -0300

    style: change autocomplete styles

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -5,12 +5,26 @@ import * as ButtonStyles from 'components/Button/styles';
 import * as TextFieldStyles from 'components/TextField/styles';
 
 export const FormWrapper = styled.form`
-  ${({ theme }) => css`
+  ${({ theme: { colors, spacings } }) => css`
     ${TextFieldStyles.Wrapper} {
-      margin: ${theme.spacings.xxsmall} 0;
+      margin: ${spacings.xxsmall} 0;
     }
     ${ButtonStyles.Container} {
-      margin: ${theme.spacings.medium} auto ${theme.spacings.xsmall};
+      margin: ${spacings.medium} auto ${spacings.xsmall};
+    }
+
+    input:-webkit-autofill,
+    input:-webkit-autofill:hover,
+    input:-webkit-autofill:focus,
+    textarea:-webkit-autofill,
+    textarea:-webkit-autofill:hover,
+    textarea:-webkit-autofill:focus,
+    select:-webkit-autofill,
+    select:-webkit-autofill:hover,
+    select:-webkit-autofill:focus {
+      border: none;
+      -webkit-text-fill-color: ${colors.black};
+      -webkit-box-shadow: 0 0 0px 1000px ${colors.lightGray} inset;
     }
   `}
 `;
