8ec37d5946f161dedb67e97d5a420be5b6ba3d1b
luizeboli <unifelicio@gmail.com>
2020-11-05 11:37:05 -0300

    test: add logo test to cover id

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -16,6 +16,12 @@ describe('<Logo />', () => {
     });
   });
 
+  it('should render the logo with id passed', () => {
+    const { container } = renderWithTheme(<Logo id="id" />);
+
+    expect(container.querySelector('#paint_linear_id')).toBeInTheDocument();
+  });
+
   it('should render a black label when color is passed', () => {
     renderWithTheme(<Logo color="black" />);
 
