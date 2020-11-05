68e1f1b27232450a761bf3aa39c29a0f6f0237f2
luizeboli <unifelicio@gmail.com>
2020-11-05 13:17:09 -0300

    feat: add sign in and sign up pages

---
+++ src/pages/sign-in.tsx
@@ -0,0 +1,11 @@
+import Auth from 'screens/Auth';
+
+import FormSignIn from 'components/FormSignIn';
+
+export default function SignIn() {
+  return (
+    <Auth title="Sign In">
+      <FormSignIn />
+    </Auth>
+  );
+}
---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,11 @@
+import Auth from 'screens/Auth';
+
+import FormSignUp from 'components/FormSignUp';
+
+export default function SignUp() {
+  return (
+    <Auth title="Sign Up">
+      <FormSignUp />
+    </Auth>
+  );
+}
