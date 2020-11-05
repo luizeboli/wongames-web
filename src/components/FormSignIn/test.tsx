0a7ca9c5442fc65b6dfc363276554f093e996628
luizeboli <unifelicio@gmail.com>
2020-11-05 12:24:36 -0300

    test: add sign in form test

--- src/components/FormSignIn/test.tsx
+++ src/components/FormSignIn/test.tsx
@@ -1,5 +1,32 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+
+import { renderWithTheme } from 'utils/tests/helpers';
 
 import FormSignIn from '.';
 
-describe('<FormSignIn />', () => {});
+describe('<FormSignIn />', () => {
+  it('should render the form', () => {
+    renderWithTheme(<FormSignIn />);
+
+    expect(screen.getByPlaceholderText(/e-mail/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /sign in/i }),
+    ).toBeInTheDocument();
+  });
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />);
+
+    expect(
+      screen.getByRole('link', { name: /forgot your password\?/i }),
+    ).toBeInTheDocument();
+  });
+
+  it('should render text to sign up if already have an account', () => {
+    renderWithTheme(<FormSignIn />);
+
+    expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument();
+    expect(screen.getByText(/don’t have an account\?/i)).toBeInTheDocument();
+  });
+});
