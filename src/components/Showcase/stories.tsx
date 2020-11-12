7d4c20396a6945e03c51e148dbebaad16e92a1e6
luizeboli <unifelicio@gmail.com>
2020-11-12 16:20:07 -0300

    feat: add showcase component

---
+++ src/components/Showcase/stories.tsx
@@ -0,0 +1,59 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+
+import gamesMock from 'components/GameCardSlider/mock';
+import highlightMock from 'components/Highlight/mock';
+
+import Showcase, { ShowcaseProps } from '.';
+
+export default {
+  title: 'Showcase',
+  component: Showcase,
+  decorators: [
+    (Story) => (
+      <div style={{ margin: '0 auto' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark',
+    },
+  },
+} as Meta;
+
+export const Basic: Story<ShowcaseProps> = (args) => <Showcase {...args} />;
+
+Basic.args = {
+  title: 'Most popular',
+  highlight: highlightMock,
+  games: gamesMock,
+};
+
+export const withoutTitle: Story<ShowcaseProps> = (args) => (
+  <Showcase {...args} />
+);
+
+withoutTitle.args = {
+  highlight: highlightMock,
+  games: gamesMock,
+};
+
+export const withoutHighlight: Story<ShowcaseProps> = (args) => (
+  <Showcase {...args} />
+);
+
+Basic.args = {
+  title: 'Most popular',
+  games: gamesMock,
+};
+
+export const withoutGames: Story<ShowcaseProps> = (args) => (
+  <Showcase {...args} />
+);
+
+withoutGames.args = {
+  title: 'Most popular',
+  highlight: highlightMock,
+};
---
+++ src/components/Showcase/test.tsx
@@ -0,0 +1,5 @@
+import { render, screen } from '@testing-library/react';
+
+import Showcase from '.';
+
+describe('<Showcase />', () => {});
