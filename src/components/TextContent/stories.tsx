e7a8ae64dd4a352da2bedc1162d9d6ab753f53a5
luizeboli <unifelicio@gmail.com>
2020-11-13 10:23:07 -0300

    feat: add textcontent component

---
+++ src/components/TextContent/stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+
+import textMock from './mock';
+
+import TextContent, { TextContentProps } from '.';
+
+export default {
+  title: 'TextContent',
+  component: TextContent,
+  args: textMock,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark',
+    },
+  },
+} as Meta;
+
+export const Default: Story<TextContentProps> = (args) => (
+  <TextContent {...args} />
+);
