c167b85b0054d4175931933db53f2ee53dcd8189
Luiz Felicio <unifelicio@gmail.com>
2020-11-27 10:36:50 -0300

    feat: add wishlist page structure

--- src/screens/Layout/styles.ts
+++ src/screens/Layout/styles.ts
@@ -9,7 +9,10 @@ export const Wrapper = styled.div`
 `;
 
 export const Content = styled.div`
-  flex: 1 0 auto;
+  ${({ theme: { spacings } }) => css`
+    flex: 1 0 auto;
+    margin-top: ${spacings.large};
+  `}
 `;
 
 export const SectionFooter = styled.section`
