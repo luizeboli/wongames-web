1d92d68b9b17cc35bfd9a01f128246744a143a46
Luiz Felicio <unifelicio@gmail.com>
2020-12-04 12:35:58 -0300

    feat: add payment options component

--- src/components/PaymentOptions/stories.tsx
+++ src/components/PaymentOptions/stories.tsx
@@ -10,8 +10,23 @@ export default {
   args: {
     cards: cardsMock,
   },
+  argTypes: {
+    cards: {
+      type: '',
+    },
+    handlePayment: {
+      action: 'clicked',
+    },
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark',
+    },
+  },
 } as Meta;
 
 export const Default: Story<PaymentOptionsProps> = (args) => (
-  <PaymentOptions {...args} />
+  <div style={{ padding: 16, maxWidth: 400 }}>
+    <PaymentOptions {...args} />
+  </div>
 );
