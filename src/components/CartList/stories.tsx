05080589e8c24023fe5f184b27623e0e261b6bfc
Luiz Felicio <unifelicio@gmail.com>
2020-12-04 12:28:41 -0300

    feat: add cartlist component

---
+++ src/components/CartList/stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+
+import mockItems from './mock';
+
+import CartList, { CartListProps } from '.';
+
+export default {
+  title: 'CartList',
+  component: CartList,
+  args: {
+    items: mockItems,
+    total: 'R$ 330,00',
+  },
+  argTypes: {
+    items: {
+      type: '',
+    },
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark',
+    },
+  },
+} as Meta;
+
+export const Default: Story<CartListProps> = (args) => (
+  <div style={{ maxWidth: 800 }}>
+    <CartList {...args} />
+  </div>
+);
---
+++ src/components/CartList/test.tsx
@@ -0,0 +1,20 @@
+import { screen } from '@testing-library/react';
+
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import mockItems from './mock';
+
+import CartList from '.';
+
+describe('<CartList />', () => {
+  it('should render the cart list', () => {
+    const { container } = renderWithTheme(
+      <CartList items={mockItems} total="R$ 330,00" />,
+    );
+
+    expect(screen.getAllByRole('heading')).toHaveLength(2);
+    expect(screen.getByText('R$ 330,00')).toHaveStyle({ color: '#F231A5' });
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
