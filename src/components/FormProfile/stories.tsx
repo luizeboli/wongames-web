0712c7cd7b38ad95128751160dc13132c6133c2b
Luiz Felicio <unifelicio@gmail.com>
2020-12-07 13:30:15 -0300

    feat: add form profile component structure

---
+++ src/components/FormProfile/stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+
+import FormProfile from '.';
+
+export default {
+  title: 'FormProfile',
+  component: FormProfile,
+} as Meta;
+
+export const Default: Story = () => <FormProfile />;
