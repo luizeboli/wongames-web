64e12830b70d9796749597bfbe11d3e91082b357
Luiz Felicio <unifelicio@gmail.com>
2021-01-04 15:23:25 -0300

    feat: add profile page

---
+++ src/pages/profile/me.tsx
@@ -0,0 +1,11 @@
+import Profile from 'screens/Profile';
+
+import FormProfile from 'components/FormProfile';
+
+export default function Me() {
+  return (
+    <Profile>
+      <FormProfile />
+    </Profile>
+  );
+}
