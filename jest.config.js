8c82c2c1c133f5ec283c0661d0532fcaf4a1d031
Luiz Felicio <unifelicio@gmail.com>
2021-01-28 13:29:06 -0300

    chore: adjust jest config

--- jest.config.js
+++ jest.config.js
@@ -7,11 +7,12 @@ module.exports = {
     '!src/**/stories.tsx',
     '!src/pages/**/*.tsx',
     '!src/styles/**/*.ts',
+    '!src/utils/apollo.ts',
+    '!src/types/**/*.ts',
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest/'],
   moduleNameMapper: {
-    '^styled-components':
-      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js',
+    '^styled-components': '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js',
   },
 };
