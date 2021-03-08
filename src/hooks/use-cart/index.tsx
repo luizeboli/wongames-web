073237392d4ecaa7a0e83ad6e6d4363abfeb09db
Luiz Felicio <unifelicio@gmail.com>
2021-03-08 12:46:43 -0300

    test: add initial test for cart context

--- src/hooks/use-cart/index.tsx
+++ src/hooks/use-cart/index.tsx
@@ -1,17 +1,33 @@
-import { createContext, ReactNode, useContext } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
 
-export type CartContextData = {};
+import { getStorageItem } from 'utils/localStorage';
 
-export const CartContextDefaultValues = {};
+export type CartContextData = {
+  items: string[];
+};
 
-export const CartContext = createContext<CartContextData, CartContextDefaultValues>();
+export const CartContextDefaultValues = {
+  items: [],
+};
+
+export const CartContext = createContext<CartContextData>(CartContextDefaultValues);
 
 export type CartProviderProps = {
   children: ReactNode;
 };
 
 export const CartProvider = ({ children }: CartProviderProps) => {
-  return <CartContext.Provider value={{}}>{children}</CartContext.Provider>;
+  const [cartItems, setCartItems] = useState<string[]>([]);
+
+  useEffect(() => {
+    const data = getStorageItem('cartItems');
+
+    if (data) {
+      setCartItems(data);
+    }
+  }, []);
+
+  return <CartContext.Provider value={{ items: cartItems }}>{children}</CartContext.Provider>;
 };
 
 export const useCart = () => useContext(CartContext);
