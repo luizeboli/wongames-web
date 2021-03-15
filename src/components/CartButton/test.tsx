b12f79e81118f83ad4b1382b5881387755d693d5
Luiz Felicio <unifelicio@gmail.com>
2021-03-15 17:18:53 -0300

    feat: add cartbutton component

---
+++ src/components/CartButton/test.tsx
@@ -0,0 +1,42 @@
+import userEvent from '@testing-library/user-event';
+
+import { CartContextDefaultValues } from 'hooks/use-cart';
+import { render, screen } from 'utils/test-utils';
+
+import CartButton from '.';
+
+describe('<CartButton />', () => {
+  it('should render button with add icon and call add method', () => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      isInCart: () => false,
+      addToCart: jest.fn(),
+    };
+
+    render(<CartButton id="1" />, { cartProviderProps });
+
+    const button = screen.getByLabelText(/add to cart/i);
+
+    expect(button).toBeInTheDocument();
+
+    userEvent.click(button);
+    expect(cartProviderProps.addToCart).toHaveBeenCalledWith('1');
+  });
+
+  it('should render button with remove icon and call remove method', () => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      isInCart: () => true,
+      removeFromCart: jest.fn(),
+    };
+
+    render(<CartButton id="2" />, { cartProviderProps });
+
+    const button = screen.getByLabelText(/remove from cart/i);
+
+    expect(button).toBeInTheDocument();
+
+    userEvent.click(button);
+    expect(cartProviderProps.removeFromCart).toHaveBeenCalledWith('2');
+  });
+});
