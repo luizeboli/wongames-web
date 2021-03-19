90f8b28a855ede7e754ef4bc770f732d337fcec9
Luiz Felicio <unifelicio@gmail.com>
2021-03-19 14:22:19 -0300

    feat: add loading and signin after signup

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import Link from 'next/link';
+import { signIn } from 'next-auth/client';
 import { useMutation } from '@apollo/client';
 import { AccountCircle, Email, Lock } from '@styled-icons/material-outlined';
 
 import Button from 'components/Button';
-import { FormLink, FormWrapper } from 'components/Form';
+import { FormLink, FormLoading, FormWrapper } from 'components/Form';
 import TextField from 'components/TextField';
 import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes';
 import { MUTATION_REGISTER } from 'graphql/mutations/register';
@@ -16,7 +17,14 @@ const FormSignUp = () => {
     password: '',
   });
 
-  const [createUser] = useMutation(MUTATION_REGISTER);
+  const [createUser, { error, loading }] = useMutation(MUTATION_REGISTER, {
+    onCompleted: () => {
+      if (!error) {
+        signIn('credentials', { email: values.email, password: values.password, callbackUrl: '/' });
+      }
+    },
+    onError: (error) => console.log(error),
+  });
 
   const handleInput = (inputName: string, value: string) => {
     setValues((prev) => ({ ...prev, [inputName]: value }));
@@ -59,8 +67,8 @@ const FormSignUp = () => {
         icon={<Lock />}
       />
 
-      <Button type="submit" size="large" fullWidth>
-        Sign up now
+      <Button type="submit" size="large" disabled={loading} fullWidth>
+        {loading ? <FormLoading /> : 'Sign up now'}
       </Button>
 
       <FormLink>
