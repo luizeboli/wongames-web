1d59ff10b0561100050998124ba850e09db37e41
Luiz Felicio <unifelicio@gmail.com>
2021-03-19 14:32:17 -0300

    feat: add signout to profile menu

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { signOut } from 'next-auth/client';
 import { AccountCircle, CreditCard, ExitToApp, FormatListBulleted } from '@styled-icons/material-outlined';
 
 import * as S from './styles';
@@ -30,12 +31,10 @@ const ProfileMenu = ({ activeLink }: ProfileMenuProps) => (
       </S.Link>
     </Link>
 
-    <Link href="/logout" passHref>
-      <S.Link title="Sign out">
-        <ExitToApp size={24} />
-        <span>Sign out</span>
-      </S.Link>
-    </Link>
+    <S.Link role="button" onClick={() => signOut()} title="Sign out">
+      <ExitToApp size={24} />
+      <span>Sign out</span>
+    </S.Link>
   </S.Nav>
 );
 
