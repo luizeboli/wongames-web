cc3a77e9c6b6afd9d3244d267b423d5053a887e0
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-05-14 18:26:59 -0300

    feat: add spinner component to wishlist button

--- src/components/WishlistButton/index.tsx
+++ src/components/WishlistButton/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { useSession } from 'next-auth/client';
 import { Favorite, FavoriteBorder } from '@styled-icons/material-outlined';
 
 import Button, { ButtonProps } from 'components/Button';
+import Spinner from 'components/Spinner';
 import { useWishlist } from 'hooks/use-wishlist';
 
 export type WishlistButtonProps = {
@@ -10,12 +12,15 @@ export type WishlistButtonProps = {
 } & Pick<ButtonProps, 'size'>;
 
 const WishlistButton = ({ gameId, hasText = false, size = 'small' }: WishlistButtonProps) => {
+  const [loading, setLoading] = useState(false);
   const { isInWishlist, removeFromWishlist, addToWishlist } = useWishlist();
   const [session] = useSession();
   const idIsInWishlist = isInWishlist(gameId);
 
-  const handleClick = () => {
-    idIsInWishlist ? removeFromWishlist(gameId) : addToWishlist(gameId);
+  const handleClick = async () => {
+    setLoading(true);
+    idIsInWishlist ? await removeFromWishlist(gameId) : await addToWishlist(gameId);
+    setLoading(false);
   };
 
   const buttonText = idIsInWishlist ? 'Remove from wishlist' : 'Add to wishlist';
@@ -25,7 +30,7 @@ const WishlistButton = ({ gameId, hasText = false, size = 'small' }: WishlistBut
   return (
     <Button
       onClick={handleClick}
-      icon={idIsInWishlist ? <Favorite aria-label={buttonText} /> : <FavoriteBorder aria-label={buttonText} />}
+      icon={loading ? <Spinner /> : idIsInWishlist ? <Favorite aria-label={buttonText} /> : <FavoriteBorder aria-label={buttonText} />}
       minimal
       size={size}
     >
