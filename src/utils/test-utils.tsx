3de75c99d9223e2f6ac76bbef48f8fe58c1db445
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-05-14 17:47:32 -0300

    feat: add wishlist provider to render utils

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -3,16 +3,27 @@ import { ThemeProvider } from 'styled-components';
 import { render, RenderOptions } from '@testing-library/react';
 
 import { CartContext, CartContextData, CartContextDefaultValues } from 'hooks/use-cart';
+import { WishlistContext, WishlistContextData, WishlistContextDefaultValues } from 'hooks/use-wishlist';
 import theme from 'styles/theme';
 
 type CustomRenderProps = {
   cartProviderProps?: CartContextData;
+  wishlistProviderProps?: WishlistContextData;
 } & Omit<RenderOptions, 'queries'>;
 
-const customRender = (ui: ReactElement, { cartProviderProps = CartContextDefaultValues, ...renderOptions }: CustomRenderProps = {}) =>
+const customRender = (
+  ui: ReactElement,
+  {
+    cartProviderProps = CartContextDefaultValues,
+    wishlistProviderProps = WishlistContextDefaultValues,
+    ...renderOptions
+  }: CustomRenderProps = {},
+) =>
   render(
     <ThemeProvider theme={theme}>
-      <CartContext.Provider value={cartProviderProps}>{ui}</CartContext.Provider>
+      <WishlistContext.Provider value={wishlistProviderProps}>
+        <CartContext.Provider value={cartProviderProps}>{ui}</CartContext.Provider>
+      </WishlistContext.Provider>
     </ThemeProvider>,
     renderOptions,
   );
