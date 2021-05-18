46775afe073d96b4ecc3644ecb989f179ec4c313
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-05-18 10:32:33 -0300

    refactor: get profile by session id

--- src/pages/profile/me.tsx
+++ src/pages/profile/me.tsx
@@ -1,7 +1,7 @@
 import { GetServerSidePropsContext } from 'next';
 
 import FormProfile, { FormProfileProps } from 'components/FormProfile';
-import { QueryProfileMe } from 'graphql/generated/QueryProfileMe';
+import { QueryProfileMe, QueryProfileMeVariables } from 'graphql/generated/QueryProfileMe';
 import { QUERY_PROFILE_ME } from 'graphql/queries/profile';
 import Profile from 'screens/Profile';
 import { initializeApollo } from 'utils/apollo';
@@ -19,17 +19,18 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context);
   const apolloClient = initializeApollo(null, session);
 
-  console.log({ session });
-
-  const { data } = await apolloClient.query<QueryProfileMe>({
+  const { data } = await apolloClient.query<QueryProfileMe, QueryProfileMeVariables>({
     query: QUERY_PROFILE_ME,
+    variables: {
+      identifier: session?.id as string,
+    },
   });
 
   return {
     props: {
       session,
-      username: data?.me?.username,
-      email: data?.me?.email,
+      username: data?.user?.username,
+      email: data?.user?.email,
     },
   };
 }
