299799b596b147d5e4929d73aa29117f1bd1e954
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-05-18 12:52:14 -0300

    feat: protect cart page

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,3 +1,5 @@
+import { GetServerSidePropsContext } from 'next';
+
 import itemsMock from 'components/CartList/mock';
 import cardsMock from 'components/PaymentOptions/mock';
 import { QueryRecommended } from 'graphql/generated/QueryRecommended';
@@ -5,13 +7,15 @@ import { QUERY_RECOMMENDED } from 'graphql/queries/recommended';
 import Cart, { CartProps } from 'screens/Cart';
 import { initializeApollo } from 'utils/apollo';
 import { gamesMapper, highlightMapper } from 'utils/mappers';
+import protectedRoutes from 'utils/protected-routes';
 
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />;
 }
 
-export async function getServerSideProps() {
-  const apolloClient = initializeApollo();
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const session = await protectedRoutes(ctx);
+  const apolloClient = initializeApollo(null, session);
 
   const { data } = await apolloClient.query<QueryRecommended>({ query: QUERY_RECOMMENDED });
 
@@ -23,6 +27,7 @@ export async function getServerSideProps() {
       recommendedTitle: data.recommended?.section?.title,
       recommendedGames: gamesMapper(data.recommended?.section?.games),
       recommendedHighlight: highlightMapper(data.recommended?.section?.highlight),
+      session,
     },
   };
 }
