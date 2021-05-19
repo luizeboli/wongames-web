c52b84fb14c0acaa1771050a7be52ba492e3df77
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-05-19 13:24:48 -0300

    feat: improve signout flow to redirect to home

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { signOut } from 'next-auth/client';
 import { AccountCircle, ExitToApp, FormatListBulleted } from '@styled-icons/material-outlined';
 
@@ -8,27 +9,36 @@ export type ProfileMenuProps = {
   activeLink?: '/profile/me' | '/profile/cards' | '/profile/orders' | string;
 };
 
-const ProfileMenu = ({ activeLink }: ProfileMenuProps) => (
-  <S.Nav>
-    <Link href="/profile/me" passHref>
-      <S.Link isActive={activeLink === '/profile/me'} title="My profile">
-        <AccountCircle size={24} />
-        <span>My profile</span>
-      </S.Link>
-    </Link>
+const ProfileMenu = ({ activeLink }: ProfileMenuProps) => {
+  const router = useRouter();
 
-    <Link href="/profile/orders" passHref>
-      <S.Link isActive={activeLink === '/profile/orders'} title="My orders">
-        <FormatListBulleted size={24} />
-        <span>My orders</span>
-      </S.Link>
-    </Link>
+  const handleSignOut = async () => {
+    const data = await signOut({ redirect: false, callbackUrl: '/' });
+    router.push(data.url);
+  };
 
-    <S.Link role="button" onClick={() => signOut()} title="Sign out">
-      <ExitToApp size={24} />
-      <span>Sign out</span>
-    </S.Link>
-  </S.Nav>
-);
+  return (
+    <S.Nav>
+      <Link href="/profile/me" passHref>
+        <S.Link isActive={activeLink === '/profile/me'} title="My profile">
+          <AccountCircle size={24} />
+          <span>My profile</span>
+        </S.Link>
+      </Link>
+
+      <Link href="/profile/orders" passHref>
+        <S.Link isActive={activeLink === '/profile/orders'} title="My orders">
+          <FormatListBulleted size={24} />
+          <span>My orders</span>
+        </S.Link>
+      </Link>
+
+      <S.Link role="button" onClick={handleSignOut} title="Sign out">
+        <ExitToApp size={24} />
+        <span>Sign out</span>
+      </S.Link>
+    </S.Nav>
+  );
+};
 
 export default ProfileMenu;
