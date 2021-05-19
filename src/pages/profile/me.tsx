00c3db8dd624f89b27c7f23eb17ee7c81f2a6dc7
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-05-19 13:20:48 -0300

    fix: early return if theres no session

--- src/pages/profile/me.tsx
+++ src/pages/profile/me.tsx
@@ -19,6 +19,12 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context);
   const apolloClient = initializeApollo(null, session);
 
+  if (!session) {
+    return {
+      props: {},
+    };
+  }
+
   const { data } = await apolloClient.query<QueryProfileMe, QueryProfileMeVariables>({
     query: QUERY_PROFILE_ME,
     variables: {
