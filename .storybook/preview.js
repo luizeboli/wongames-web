e62edd7f540981b5e4a882ed27aa4c0b79cb4810
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-05-19 13:46:09 -0300

    test: add mock for next router

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,10 @@
-import { ThemeProvider } from 'styled-components';
-
-import GlobalStyles from 'styles/global';
-import theme from 'styles/theme';
+import '../.jest/next-image.mock'
+import { addDecorator } from '@storybook/react'
+import { ThemeProvider } from 'styled-components'
 import { CartContext, CartContextDefaultValues } from 'hooks/use-cart'
+import { withNextRouter } from 'storybook-addon-next-router'
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 export const parameters = {
   backgrounds: {
@@ -20,13 +22,21 @@ export const parameters = {
   }
 }
 
+addDecorator(withNextRouter())
+
 export const decorators = [
-  (Story) => (
+  (Story, context) => (
     <ThemeProvider theme={theme}>
-      <CartContext.Provider value={{...CartContextDefaultValues, ...(context?.args?.cartContextValue || {}), ...context.args}}>
-      <GlobalStyles removeBg />
-      <Story />
+      <CartContext.Provider
+        value={{
+          ...CartContextDefaultValues,
+          ...(context?.args?.cartContextValue || {}),
+          ...context.args
+        }}
+      >
+        <GlobalStyles removeBg />
+        <Story />
       </CartContext.Provider>
     </ThemeProvider>
-  ),
-];
+  )
+]
