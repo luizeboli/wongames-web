2144c735fb3d2d5c897960fd8bac2e6236f5e110
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-05-19 13:26:39 -0300

    feat: improve signout flow to redirect to home

--- src/components/UserDropdown/index.tsx
+++ src/components/UserDropdown/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { signOut } from 'next-auth/client';
 import { ChevronDown } from '@styled-icons/boxicons-regular/ChevronDown';
 import { AccountCircle, ExitToApp, FavoriteBorder } from '@styled-icons/material-outlined';
@@ -11,36 +12,45 @@ export type UserDropdownProps = {
   username: string;
 };
 
-const UserDropdown = ({ username }: UserDropdownProps) => (
-  <Dropdown
-    title={
-      <>
-        <AccountCircle size={24} />
-        <S.Username>{username}</S.Username>
-        <ChevronDown size={24} />
-      </>
-    }
-  >
-    <S.Nav>
-      <Link href="/profile/me" passHref>
-        <S.Link>
-          <AccountCircle />
-          <span>My profile</span>
-        </S.Link>
-      </Link>
-      <Link href="/wishlist" passHref>
-        <S.Link title="Wishlist">
-          <FavoriteBorder />
-          <span>Wishlist</span>
-        </S.Link>
-      </Link>
+const UserDropdown = ({ username }: UserDropdownProps) => {
+  const router = useRouter();
 
-      <S.Link role="button" onClick={() => signOut()} title="Sign out">
-        <ExitToApp />
-        <span>Sign out</span>
-      </S.Link>
-    </S.Nav>
-  </Dropdown>
-);
+  const handleSignOut = async () => {
+    const data = await signOut({ redirect: false, callbackUrl: '/' });
+    router.push(data.url);
+  };
+
+  return (
+    <Dropdown
+      title={
+        <>
+          <AccountCircle size={24} />
+          <S.Username>{username}</S.Username>
+          <ChevronDown size={24} />
+        </>
+      }
+    >
+      <S.Nav>
+        <Link href="/profile/me" passHref>
+          <S.Link>
+            <AccountCircle />
+            <span>My profile</span>
+          </S.Link>
+        </Link>
+        <Link href="/wishlist" passHref>
+          <S.Link title="Wishlist">
+            <FavoriteBorder />
+            <span>Wishlist</span>
+          </S.Link>
+        </Link>
+
+        <S.Link role="button" onClick={handleSignOut} title="Sign out">
+          <ExitToApp />
+          <span>Sign out</span>
+        </S.Link>
+      </S.Nav>
+    </Dropdown>
+  );
+};
 
 export default UserDropdown;
