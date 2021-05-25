56b99d00cc41bcb6857755c9709b1838ac5c685c
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2021-05-25 08:42:51 -0300

    fix: adjust footer access

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -17,12 +17,12 @@ const Footer = () => (
         <a href="mailto:sac@wongames.com">sac@wongames.com</a>
       </S.Column>
 
-      <S.Column>
+      <S.Column aria-labelledby="social-media">
         <Heading color="black" lineColor="secondary" lineBottom size="small">
           Follow us
         </Heading>
 
-        <nav aria-labelledby="social media">
+        <nav id="social-media">
           <a href="https://www.instagram.com/won-games" target="_blank" rel="noopenner, noreferrer">
             Instagram
           </a>
@@ -38,12 +38,12 @@ const Footer = () => (
         </nav>
       </S.Column>
 
-      <S.Column>
+      <S.Column aria-labelledby="footer-resources">
         <Heading color="black" lineColor="secondary" lineBottom size="small">
           Links
         </Heading>
 
-        <nav aria-labelledby="footer resources">
+        <nav id="footer-resources">
           <Link href="/">
             <a>Home</a>
           </Link>
@@ -56,7 +56,7 @@ const Footer = () => (
         </nav>
       </S.Column>
 
-      <S.Column aria-labelledby="footer-contact">
+      <S.Column aria-label="contact">
         <Heading color="black" lineColor="secondary" lineBottom size="small">
           Location
         </Heading>
