b3d6bb697aa92c83f7370e5f27214f61db25357e
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2022-08-16 11:41:07 -0300

    fix(preparing-to-production): graphql variable identifier

--- src/graphql/queries/profile.ts
+++ src/graphql/queries/profile.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 export const QueryProfileMe = gql`
   query QueryProfileMe($identifier: ID!) {
-    usersPermissionsUser(id: $$identifier) {
+    usersPermissionsUser(id: $identifier) {
       data {
         id
         attributes {
